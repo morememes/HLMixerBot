f3659c0437c6cd8835e0f0e0f3ae47f89b651b2a
morememes <theblackfridge1001@gmail.com>
2019-06-21 15:50:29 +0300

    add remove func, update mixready func

--- hlmixbot.py
+++ hlmixbot.py
@@ -4,25 +4,39 @@ from discord.ext.commands import Bot
 
 from libs.core import Mix, Mixer
 
+
+
 Bot = commands.Bot(command_prefix = '!')
+mixchannel = None
+
 
 @Bot.event
 async def on_ready():
+    global mix
     print ("pahan upal v yamu")
+    channels = Bot.get_all_channels()
+    for channel in channels:
+        if channel.name == 'микс':
+            mixchannel = channel
+            break
+    mix = Mixer(mixchannel=mixchannel)
 
-mix = Mixer()
+
 
 @Bot.event
 async def on_message(message):
+    global mix
     if not message.author.bot:
         if message.content == "!create mix":
             mix.create_mix()
         elif message.content == "!add mix" or message.content == "++":
-            mix.add_member(message.author.name)
+            await mix.add_member(message)
+        elif message.content == "!rep me" or message.content == "--":
+            await mix.remove_member(message)
         elif message.content == "!who":
             await message.channel.send(mix.get_members())
 
-Bot.run("NTkxMjI2OTY3ODYxMzYyNzA3.XQttEA.8vUXFr3H2SDeV3ptDPngXTqPK7Q")
+Bot.run("NTkxMjI2OTY3ODYxMzYyNzA3.XQy6qg.t49DoYxD9vcR_Dmn6sppjjETzFU")
 
 
 
--- libs/core.py
+++ libs/core.py
@@ -2,36 +2,80 @@
 #from discord.ext import commands
 #from discord.ext.commands import Bot
 
+async def send_msg(channel, msg):
+    await channel.send(msg)
+
 class Mix():
-    def __init__(self):
+    def __init__(self, mixName = None, max_members = 2, mixchannel = None):
+        self.mixName = mixName
         self.members = []
+        self.members_id = []
         self.count_members = None
-        self.max_members = 18
+        self.max_members = max_members
+        self.ready = False
+        self.mixchannel = mixchannel
 
-    def add_member(self, nick):
-        self.members.append(nick)
+        self.timestartmsg = 'Начало в 20 мск / 19 cest!'
 
-    def remove_member(self, nick):
-        pass
+        self.blue = []
+        self.read = []
+        
+
+    async def add_member(self, message):
+        if not self.ready:
+            if not message.author.id in self.members_id:
+                self.members.append(message.author)
+                self.members_id.append(message.author.id)
+                await message.channel.send('[**Mix** ({0}/{1})]'.format(len(self.members), self.max_members))
+                if len(self.members) == self.max_members:
+                    await self.mix_ready()
+            else:
+                await message.channel.send(message.author.mention + ' , ты уже записан!')
+
+    async def remove_member(self, message):
+        if not self.ready:
+            if message.author.id in self.members_id:
+                ind = self.members_id.index(message.author.id)
+                self.members_id.pop(ind)
+                self.members.pop(ind)
+                await message.channel.send('[**Mix** ({0}/{1})]'.format(len(self.members), self.max_members))
+            else:
+                await message.channel.send(message.author.mention + ' , ты не записывался на микс!')
 
     def get_members(self):
-        st = 'Mix({0}/{1}) '.format(len(self.members), self.max_members)
+        st = '[**Mix** ({0}/{1})] '.format(len(self.members), self.max_members)
+
         for member in self.members:
-            st += '`' + member + '`/'
+            st += '`' + member.name + '`/'
         return st[:-1]
 
+    async def mix_ready(self):
+        self.ready = True
+        st = 'Микс собрался! {0} \n'.format(self.timestartmsg) # timestartmsg
+        for member in self.members:
+            st += '`' + member.name + '`\n'
+            await member.send(self.timestartmsg)
+
+        await self.mixchannel.send(st)
+        # отправка сообщений в пм и в канал миксов
+
+
+
 class Mixer():
-    def __init__(self):
+    def __init__(self, mixchannel = None):
         self.mix = None
+        self.mixchannel = mixchannel
 
     def create_mix(self):
-        self.mix = Mix()
+        self.mix = Mix(mixchannel = self.mixchannel)
 
-    def add_member(self, nick):
-        self.mix.add_member(nick)
+    async def add_member(self, message):
+        if self.mix == None:
+            self.create_mix()
+        await self.mix.add_member(message)
 
-    def remove_member(self, nick):
-        pass
+    async def remove_member(self, message):
+        await self.mix.remove_member(message)
 
     def get_members(self):
         return self.mix.get_members()
